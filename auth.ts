3b120e9aac869fd668459c2998a9484855c5d845
Ammar1999y <thesyclashers.911@gmail.com>
2024-06-23 22:44:52 +0300

    first commit

--- auth.ts
+++ auth.ts
@@ -7,8 +7,8 @@ import { adminDb, authAdmin } from "./firebase-admin";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {
